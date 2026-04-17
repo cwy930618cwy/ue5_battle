e9238bb030e4256d3f490e76c221986367c9eeb3
v_vweiychen <v_vweiychen@tencent.com>
2026-04-17 21:41:37 +0800

    1111

---
+++ Source/ue5_battle/ue5_battle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ue5_battle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue5_battle, "ue5_battle" );
